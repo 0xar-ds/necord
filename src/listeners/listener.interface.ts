cb39042d7e5b28b59a719fc7cf723826ac059b14
0xar-ds <sadden-rope-sleek@duck.com>
2025-09-05 21:10:18 -0300

    feat: BASE_CLIENT_EVENT symbol & interfaces

--- src/listeners/listener.interface.ts
+++ src/listeners/listener.interface.ts
@@ -12,6 +12,19 @@ import {
 	CustomVoiceStateUpdateEvents
 } from './handlers';
 
+export type ClientEventUnion<T extends keyof ClientEvents = keyof ClientEvents> = {
+	[Event in keyof ClientEvents]: {
+		event: Event;
+		payload: ClientEvents[Event];
+	};
+}[T];
+
+export const BASE_CLIENT_EVENT = Symbol(this);
+
+export interface ClientEventContext<T extends keyof ClientEvents = keyof ClientEvents> {
+	[BASE_CLIENT_EVENT]: ClientEventUnion<T>;
+}
+
 export type NecordEvents = ClientEvents &
 	CustomChannelUpdateEvents &
 	CustomGuildAuditLogEntryCreateEvents &
